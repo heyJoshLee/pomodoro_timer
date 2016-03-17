40a918848cd32aea005297affd03c6a36cfa0a81
jleewebdev <lee45276@gmail.com>
2016-03-17 19:10:13 +0800

    V1.0 finished

--- javascripts/timer.js
+++ javascripts/timer.js
@@ -4,9 +4,12 @@ var clock = {
   $session_minus: $(".session_length .minus"),
   $session_add: $(".session_length .add"),
   timer_on: true,
-  time: 800000,
-  session_length: 10000,
-  break_length: 10000,
+  session_on: true,
+  time: 1500000,
+  session_length: 1500000,
+  break_length: 300000,
+  break_time: 300000,
+  audio: new Audio('http://sfxcontent.s3.amazonaws.com/soundfx/MorseCode.mp3'),
 
   secondsToMinutes: function(sec) {
     var mins = 0,
@@ -23,52 +26,90 @@ var clock = {
 
   timer: function() {
     if (this.timer_on) {
+      
+      if(this.session_on){
+        this.time -= 1000;
+        $("#time_left").html(this.secondsToMinutes(this.time/1000));
 
-      this.time -= 1000;
-      $("#time_left").html(this.secondsToMinutes(this.time/1000));
-      if (this.time <= 0) {
-        this.timer_on = false;
-        alert("times up")
+        if (this.time <= 0) {
+          this.session_on = false;
+          this.break_time = this.break_length;
+          $("#label").html("Break");
+          clock.audio.play();
+          alert("Time's up!")
+        }
       }
+
+      if (!this.session_on) {
+        this.break_time -= 1000;
+        $("#time_left").html(this.secondsToMinutes(this.break_time/1000));
+
+        if (this.break_time <= 0) {
+          this.session_on = true;
+          this.time = this.session_length;
+          $("#label").html("Session");
+          clock.audio.play();
+          alert("Break's up!")
+        }
+      }
+      
+      
     } 
   }
 }
 
 setInterval(clock.timer.bind(clock), 1000)
 
-$("#timer").on("click", function() {
-  console.log("turn off/on")
+$("#pause_button").on("click", function() {
   clock.timer_on = !clock.timer_on;
+  var $this = $(this);
+  if (clock.timer_on) {
+    $this.html("Pause");
+  } else {
+    $this.html("Start");
+  }
+});
+
+$("#reset_button").on("click", function() {
+  clock.time = clock.session_length;
+  clock.break_time = clock.break_length;
+  $("#label").html("Session");
+  clock.session_on = true;
+  $("#time_left").html(clock.secondsToMinutes(clock.time/1000));
+
 });
 
 $(".break_length .minus").on("click", function() {
-  if (clock.timer_on && clock.break_length >= 1000){
-      clock.break_length -= 1000;
+  if (!clock.timer_on && clock.break_length >= 1000){
+      clock.break_length -= 1000 * 60;
     $(".break_length .time").html(clock.secondsToMinutes(clock.break_length / 1000 ));
   }
 });
 
 $(".break_length .plus").on("click", function() {
-  if (clock.timer_on){
-      clock.break_length += 1000;
+  if (!clock.timer_on){
+      clock.break_length += 1000 * 60;
     $(".break_length .time").html(clock.secondsToMinutes(clock.break_length / 1000 ));
   }
 });
 
 
 $(".session_length .minus").on("click", function() {
-  if (clock.timer_on && clock.session_length >= 1000){
-      clock.session_length -= 1000;
+  if (!clock.timer_on && clock.session_length >= 1000){
+      clock.session_length -= 1000 * 60;
     $(".session_length .time").html(clock.secondsToMinutes(clock.session_length / 1000 ));
   }
 });
 
 $(".session_length .plus").on("click", function() {
-  if (clock.timer_on){
-      clock.session_length += 1000;
+  if (!clock.timer_on){
+      clock.session_length += 1000 * 60;
     $(".session_length .time").html(clock.secondsToMinutes(clock.session_length / 1000 ));
   }
 });
+
+
+
 
 
 
